460c810098367979b2b8ccd1adbe830e4e9330fb
Prit <phirpara@primaryitcare.com>
2025-04-28 21:57:44 +0530

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U PRIT NI SAANJ ❤️ ').split('')
+    const titles = ('I LOVE U SAANJ ❤️ ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
